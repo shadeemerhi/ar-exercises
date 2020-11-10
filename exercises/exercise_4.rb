8392b27f6daf04a85934b3e332e3c64d3f0c275e
vagrant <shadmerhi@gmail.com>
2020-11-10 02:05:10 +0000

    added instance variables for last 3 stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,9 +7,9 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
-Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
-Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+@store3 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+@store4 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+@store5 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
 @mens_stores = Store.where(mens_apparel: true)
 
