d5a4b3ad5bf95494668b15eb5bfa0b3a0f48f266
vagrant <shadmerhi@gmail.com>
2020-11-10 02:18:14 +0000

    added validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3, too_short: "Name must be minimum of 3 characters" }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
