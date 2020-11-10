0b0162bb22555025eb03aed945b3acf4a323fa5d
vagrant <shadmerhi@gmail.com>
2020-11-10 03:37:49 +0000

    registered before_create callback to create random password

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,4 +2,11 @@ class Employee < ActiveRecord::Base
   belongs_to :store
   validates :first_name, :last_name, :store_id, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
+
+  before_create :generate_password
+
+  private
+  def generate_password
+    self.password = (0...8).map { (65 + rand(26)).chr }.join
+  end
 end
