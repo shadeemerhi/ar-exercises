177ce0332e39f23c4be4588418fe968882e1f8d8
vagrant <shadmerhi@gmail.com>
2020-11-10 03:54:47 +0000

    before_destroy method added to prevent destruction of non-empty stores

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,14 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3, too_short: "Name must be minimum of 3 characters" }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  before_destroy :check_if_atleast_1_employee
+
+  private
+  def check_if_atleast_1_employee
+    if employees.any?
+      errors[:base] << "Cannot delete stores with 1 or more employee!"
+      return false
+    end
+  end
 end
